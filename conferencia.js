6dd4cb4d2c8e987ccfa45f3a10c64113414c12cb
ONILDO PEREIRA DA COSTA JUNIOR <126628992+onildopr@users.noreply.github.com>
2026-02-04 22:52:52 -0400

    Update conferencia.js

--- conferencia.js
+++ conferencia.js
@@ -2171,7 +2171,7 @@ $('#extract-btn').click(() => {
   // ✅ limpa o campo após importar
   $('#html-input').val('');
 
-  alert(`${qtd} rota(s) importada(s) e salva(s)! Agora selecione e clique em "Carregar rota".`);
+  
 });
 
 // Carregar rota
@@ -2489,3 +2489,4 @@ $(document).on('click', '#export-csv-todas-rotas-placa', () => {
 $(document).on('click', '#export-csv-mapa-carretas', () => {
   ConferenciaApp.exportMapaCarretasCsv();
 });
+
