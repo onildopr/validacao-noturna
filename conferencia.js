4956769bdd18b1a0f420638e32c76a61f5a9e31a
ONILDO PEREIRA DA COSTA JUNIOR <126628992+onildopr@users.noreply.github.com>
2026-02-08 18:24:14 -0400

    sem alert

--- conferencia.js
+++ conferencia.js
@@ -2357,7 +2357,6 @@ $('#extract-btn').click(() => {
   // ✅ limpa o campo após importar
   $('#html-input').val('');
 
-  alert(`${qtd} rota(s) importada(s) e salva(s)! Agora selecione e clique em "Carregar rota".`);
 });
 
 // Carregar rota
@@ -2764,4 +2763,5 @@ $(document).on('click', '#global-back', () => {
   $('#global-interface').addClass('d-none');
   $('#initial-interface').removeClass('d-none');
 });
+
 
